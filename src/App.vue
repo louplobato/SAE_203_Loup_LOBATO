b89a0dfa1d996b089a04cc8a39c17e981e1eb7eb
llobato <louplelpro@gmail.com>
2022-05-30 11:33:34 +0200

    footer

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,35 @@
     
 
   </header>
-
   <RouterView />
+  <footer>
+    <div class="flex flex-col text-white font-text bg-black w-screen h-80 items-center justify-center pl-16 pr-16">
+      <div class="flex justify-between w-full items-center">
+        <logos></logos>
+        <div class="flex flex-col">
+          <p class="mb-5">
+            Contact
+          </p>
+
+          <p>
+            Mentions Légales
+          </p>
+        </div>
+      </div>
+      <div class="flex flex-col items-center mt-10">
+        <p>
+          New Landes Metal Fest - 2022
+        </p>
+        <div class="flex mt-4 justify-between w-36">
+          <facebook></facebook>
+          <instagram></instagram>
+          <youtube></youtube>
+          <twitter></twitter>
+        </div>
+      </div>
+    </div>
+  </footer>
+  
 </template>
 
 <script>
@@ -14,7 +41,11 @@ import logos from "./components/icons/logo-s.vue"
 import logom from "./components/icons/logo-m.vue"
 import logoxl from "./components/icons/logo-xl.vue"
 import menuicon from "./components/icons/menu.vue"
+import facebook from "./components/icons/facebook.vue"
+import instagram from "./components/icons/instagram.vue"
+import youtube from "./components/icons/youtube.vue"
+import twitter from "./components/icons/twitter.vue"
 export default {
-  components: { logos, logom, logoxl, menuicon, },
+  components: { logos, logom, logoxl, menuicon, facebook, instagram, youtube, twitter },
 };
 </script>
